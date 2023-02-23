e143f38be6d4590850cf7a79f535a8b019c495a0
Abdelghafour <ghafourragg@gmail.com>
2023-02-23 12:22:56 +0100

    made upper services section

--- src/Routes/Services.tsx
+++ src/Routes/Services.tsx
@@ -14,27 +14,53 @@ const Services = (props: Props) => {
         <div className="text">
           <h1 className="smaller-header text-stone-700">Nos services</h1>
         </div>
-        <div className="grid-container grid grid-cols-5 gap-10 w-full">
+        <div className="grid-container grid grid-cols-1 gap-5 sm:gap-4 md:gap-7 sm:grid-cols-5 lg:gap-10 w-full">
           <div className="img-cont">
-            <img src={fitness} alt="physical-health" className="h-44" />
+            <img
+              src={fitness}
+              alt="physical-health"
+              className="h-32 sm:h-28 md:h-32 lg:h-44"
+            />
             <p className="sub-img-p">santé physique</p>
           </div>
           <div className="img-cont">
-            <img src={health} alt="health" className="h-44" />
+            <img
+              src={health}
+              alt="health"
+              className="h-32 sm:h-28 md:h-32 lg:h-44"
+            />
             <p className="sub-img-p">santé mentale</p>
           </div>
           <div className="img-cont">
-            <img src={supplements} alt="supplements" className="h-44" />
+            <img
+              src={supplements}
+              alt="supplements"
+              className="h-32 sm:h-28 md:h-32 lg:h-44"
+            />
             <p className="sub-img-p">suppléments et vitamines</p>
           </div>
           <div className="img-cont">
-            <img src={nutrition} alt="nutrition" className="h-44" />
+            <img
+              src={nutrition}
+              alt="nutrition"
+              className="h-32 sm:h-28 md:h-32 lg:h-44"
+            />
             <p className="sub-img-p">nutrition</p>
           </div>
           <div className="img-cont">
-            <img src={sleep} alt="sleep" className="h-44" />
+            <img
+              src={sleep}
+              alt="sleep"
+              className="h-32 sm:h-28 md:h-32 lg:h-44"
+            />
             <p className="sub-img-p">sommeil</p>
           </div>
+        </div>
+        <div className="second-grid-container grid grid-cols-2 w-full">
+          <div className="service"></div>
+          <div className="service"></div>
+          <div className="service"></div>
+          <div className="service"></div>
         </div>
       </div>
     </div>
